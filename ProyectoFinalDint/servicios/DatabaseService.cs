b40fb7a307a433228f192495430d8ae5b9749a63
PauSG <pau.sanso@iesdoctorbalmis.com>
2022-01-28 12:54:21 +0100

    Añado el nugget para la base de datos, y la base de datos a la raiz del proyecto. Tambien añado el servicio para las conexiones con la base de datos y estoy preparando las consultas

---
+++ ProyectoFinalDint/servicios/DatabaseService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalDint.servicios
+{
+    class DatabaseService
+    {
+
+        public DatabaseService()
+        {
+
+        }
+
+        public void ObtenerClientesAll() //Tendra que devolver un array o algo
+        {
+            SqliteConnection conexion = new SqliteConnection("Data Source=parking.db");
+            conexion.Open();
+
+            SqliteCommand comando = conexion.CreateCommand();
+            comando.CommandText = @"SELECT * FROM clientes";
+
+            SqliteDataReader lector = comando.ExecuteReader();
+            if(lector.HasRows)
+            {
+                while(lector.Read())
+                {
+                    int idCliente = (int)lector["id_cliente"];
+                    string nombre = (string)lector["nombre"];
+                }
+            }
+        }
+
+    }
+}
