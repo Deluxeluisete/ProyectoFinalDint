4fa5ca665feaa5cf2171096e175f14fcfd08b3fa
Deluxeluisete <luis.suarez@iesdoctorbalmis.com>
2022-02-04 11:56:05 +0100

    subo clienbte bien configurado

--- ProyectoFinalDint/vistamodelo/GestionarClientesUserControlVM.cs
+++ ProyectoFinalDint/vistamodelo/GestionarClientesUserControlVM.cs
@@ -1,4 +1,7 @@
-﻿using ProyectoFinalDint.modelo;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using ProyectoFinalDint.modelo;
+using ProyectoFinalDint.servicios;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,34 +11,54 @@ using System.Threading.Tasks;
 
 namespace ProyectoFinalDint.vistamodelo
 {
-    class GestionarClientesUserControlVM
+    class GestionarClientesUserControlVM : ObservableObject
     {
-        private ObservableCollection<OrderInfo> _orders;
-        public ObservableCollection<OrderInfo> Orders
+        private SQLiteRepositoryEstacionamientos ServicioSQLite;
+
+        private ObservableCollection<Estacionamientos> listaEstacionamientos;
+        public ObservableCollection<Estacionamientos> ListaEstacionamientos
         {
-            get { return _orders; }
-            set { _orders = value; }
+            get => listaEstacionamientos;
+            set => SetProperty(ref listaEstacionamientos, value);
         }
 
+        private ObservableCollection<Estacionamientos> listaBinding;
+
+        public ObservableCollection<Estacionamientos> ListaBinding
+        {
+            get { return listaBinding; }
+            set { SetProperty(ref listaBinding, value); }
+        }
+
+        public RelayCommand AñadirEstacionamientoCommand { get; }
+        public RelayCommand FindAllCommand { get; }
         public GestionarClientesUserControlVM()
         {
-            _orders = new ObservableCollection<OrderInfo>();
-            this.GenerateOrders();
+            ServicioSQLite = new SQLiteRepositoryEstacionamientos();
+            this.ListaBinding = new ObservableCollection<Estacionamientos>();
+            rellenarListaBinding();
+            this.ListaEstacionamientos = ServicioSQLite.FindAll();
+            AñadirEstacionamientoCommand = new RelayCommand(AñadirEstacionamiento);
+            FindAllCommand = new RelayCommand(FindAll);
         }
 
-        private void GenerateOrders()
+        private void FindAll()
         {
-            _orders.Add(new OrderInfo(1001, "Maria Anders", "Germany", "ALFKI", "Berlin"));
-            _orders.Add(new OrderInfo(1002, "Ana Trujilo", "Mexico", "ANATR", "Mexico D.F."));
-            _orders.Add(new OrderInfo(1003, "Antonio Moreno", "Mexico", "ANTON", "Mexico D.F."));
-            _orders.Add(new OrderInfo(1004, "Thomas Hardy", "UK", "AROUT", "London"));
-            _orders.Add(new OrderInfo(1005, "Christina Berglund", "Sweden", "BERGS", "Lula"));
-            _orders.Add(new OrderInfo(1006, "Hanna Moos", "Germany", "BLAUS", "Mannheim"));
-            _orders.Add(new OrderInfo(1007, "Frederique Citeaux", "France", "BLONP", "Strasbourg"));
-            _orders.Add(new OrderInfo(1008, "Martin Sommer", "Spain", "BOLID", "Madrid"));
-            _orders.Add(new OrderInfo(1009, "Laurence Lebihan", "France", "BONAP", "Marseille"));
-            _orders.Add(new OrderInfo(1010, "Elizabeth Lincoln", "Canada", "BOTTM", "Tsawassen"));
+            ListaEstacionamientos = ServicioSQLite.FindAll();
         }
+
+        private void AñadirEstacionamiento()
+        {
+            ServicioSQLite.Inserta(new Estacionamientos(5, 8, "1234 GGE", "03/02/2022 - 12:34", "03/02/2022 - 14:23", 5.9, "coche"));
+        }
+
+        public void rellenarListaBinding()
+        {
+            listaBinding.Add(new Estacionamientos(1, 1, "aaaa", "entrada", "salida", 3.5, "coche"));
+            listaBinding.Add(new Estacionamientos(2, 12, "bbbbb", "entrada", "salida", 3.5, "coche"));
+            listaBinding.Add(new Estacionamientos(3, 15, "cccc", "entrada", "salida", 3.5, "moto"));
+        }
+
+
     }
 }
-
